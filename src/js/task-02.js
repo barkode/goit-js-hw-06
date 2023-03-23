56847b9afa07ab4e22a2748c3222c2d6842002e7
barkode <konstantinks@gmail.com>
2023-03-23 12:37:34 +0000

    add secont

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+
+const addAllIngradients = ingredients.reduce((acc, ingredient) => {
+  const addLi = document.createElement("li");
+  addLi.textContent = ingredient;
+  addLi.classList.add("item");
+  acc.push(addLi);
+  return acc;
+}, []);
+
+ingredientsList.append(...addAllIngradients);
