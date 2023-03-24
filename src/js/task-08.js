f1c1e37756d0cbddbe1a91b867ec0ff0eee61657
barkode <konstantinks@gmail.com>
2023-03-24 01:26:04 +0000

    end eigth

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", hundleSubmit);
 
+function hundleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Не всі поля було заповлено.");
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    form.reset();
+  }
+}
