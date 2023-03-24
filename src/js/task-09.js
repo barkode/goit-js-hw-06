08c05e667354e3d469e6db0d093eb053d7fc5148
barkode <konstantinks@gmail.com>
2023-03-24 01:36:12 +0000

    end ninth

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanColor = document.querySelector(".color");
+const btnColor = document.querySelector(".change-color");
+
+btnColor.addEventListener("click", changeColor);
+
+function changeColor() {
+  const rndmColor = getRandomHexColor();
+  document.body.style.backgroundColor = rndmColor;
+  spanColor.textContent = rndmColor;
+}
